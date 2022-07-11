8371e3e4626c2dfadbfc10deb76f6c6567996759
Igor Marques <49289661+IgorMarques2019@users.noreply.github.com>
2022-07-11 17:58:28 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,75 @@
+// ------------------------ Menu Mobile ------------------------
+function menuToggle() {
+  const menuIcon = document.querySelector('.menu-icon')
+  const header = document.querySelector('header')
+  const liHeader = header.querySelectorAll('a')
+
+
+  liHeader.forEach((item) => {
+    item.addEventListener('click', () => {
+      document.body.classList.toggle('over-hidden')
+      header.classList.toggle('nav-ativo')
+      menuIcon.classList.toggle('menu-open')
+      icomMnu() 
+    })
+  })
+
+
+  menuIcon.addEventListener('click', () => {
+
+    header.classList.toggle('nav-ativo')
+    document.body.classList.toggle('over-hidden')
+    menuIcon.classList.toggle('menu-open')
+    icomMnu() 
+
+  })
+
+  function icomMnu() {
+    if (menuIcon.classList.contains('menu-open')) {
+      menuIcon.setAttribute('src', "./img/icons/close.png")
+    } else {
+      menuIcon.setAttribute('src', "./img/icons/menu-closed.png")
+    }
+  }
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 360) {
+      header.classList.add('header-dark')
+    } else {
+      header.classList.remove('header-dark')
+    }
+  })
+
+}
+menuToggle()
+
+// ------------------------ Progress Bar ------------------------
+
+window.addEventListener('scroll', () => {
+  const progressBar = document.querySelector('.progress')
+  const alturaJanela = window.innerHeight;
+  let alturaJanAtual = window.pageYOffset
+  let progressBase = (alturaJanAtual * 100) / alturaJanela;
+  progressBar.setAttribute('style', `width:${progressBase}%;`)
+})
+
+// ------------------------ scroll Suave ------------------------
+function scrollSuave() {
+  const jsScroll = document.querySelectorAll('.js-scroll')
+  const metadeTela = window.innerHeight * 0.6
+
+  function scrollDetect() {
+    jsScroll.forEach((item) => {
+      const contentTop = item.getBoundingClientRect().top
+      const isVisible = (contentTop - metadeTela) < 0
+      if (isVisible) {
+        item.classList.add("ativo")
+      }
+    })
+
+
+  }
+  window.addEventListener('scroll', scrollDetect)
+}
+
+
+scrollSuave()
